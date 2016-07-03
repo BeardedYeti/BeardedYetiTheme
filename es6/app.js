d88310564a7daa6c5293403fb9df02f1a0fc5592
Colin Smith <lorddragonsbane@gmail.com>
2016-07-03 12:52:26 -0400

    Fixed JS-Build, working now

---
+++ es6/app.js
@@ -0,0 +1,3 @@
+// Expression bodies
+var odds = evens.map(v => v + 1);
+var nums = evens.map((v, i) => v + i);
--- gulpfile.js
+++ gulpfile.js
@@ -13,17 +13,21 @@ var gulp = require('gulp'),
 	uglify = require('gulp-uglify'), // Minifies JS
 	autoprefixer = require('gulp-autoprefixer'), // Autoprefixes for supported browsers
 	notify = require('gulp-notify'), // Provides notification to both the console
-	sourcemaps = require('sourcemaps'), // Provide external sourcemap files
-	
+	sourcemaps = require('gulp-sourcemaps'), // Provide external sourcemap files
+	source = require('vinyl-source-stream'),
+	streamify = require('gulp-streamify'),
+    buffer = require('vinyl-buffer'),
 //Path Sources
 
 	input = {
       'sass': 'sass/**/*.scss',
-      'es6': 'es6/**/*.js'
+      'styleSass': 'sass/style.scss',
+      'javascript': 'es6/app.js'
     },
     output = {
-      'css': 'style.css',
-      'js': 'js'
+      'css': './',
+      'javascript': 'js',
+      'maps': 'maps'
     };
 
 gulp.task('default', function() {
@@ -48,17 +52,14 @@ var onError = function(err) {
 	//Notify
 
 	gulp.task('build-css', function() {
-    	return gulp.src(input.sass)
+    	return gulp.src(input.styleSass)
     		.pipe(plumber({ errorHandler: onError }))
     		.pipe(sourcemaps.init())
-      		.pipe(sass({
-        		css: 'style.css',
-        		sass: 'sass/style.scss'
-        	}))
-      		.pipe(autoprefixer())
-      		.pipe(minifycss())
+      		.pipe(sass().on('error', sass.logError))
     		.pipe(sourcemaps.write())
-    		.pipe(gulp.dest(output.stylesheets))
+    		.pipe(autoprefixer())
+      		.pipe(minifycss())
+    		.pipe(gulp.dest(output.css))
     		.pipe(notify('SASS/SCSS compiled into CSS, autoprefixed, and minified'));
     });
 
@@ -73,26 +74,39 @@ var onError = function(err) {
 	//uglify
 	//To Folder
 	//Notify
-
-	gulp.task('build-js', function() {
-    	return browserify({entries: input.es6, debug: true})
-	      	.pipe(plumber({ errorHandler: onError }))
-	      	.transform(babelify)
-	      	.pipe(jshint())
-	      	.pipe(jshint.reporter(stylish))
-	      	.bundle()
-	      	.pipe(source('bundle.js'))
-	      	.pipe(buffer())
-	      	.pipe(uglify())
-	        .pipe(gulp.dest(output.js));
-	        .pipe(notify('ES6 compiled to JS, Linted, Bundled, and Minified'));
-    });
+    gulp.task('build-js', function () {
+	  var magic = browserify({
+	    entries: input.javascript,
+	    debug: true
+	  });
+	  magic.transform(babelify.configure({
+	  	presets: ["es2015"]
+	  }));
+	  magic.bundle()
+	  	.pipe(plumber({ errorHandler: onError }))
+	    .pipe(source('bundle.js'))
+	    .pipe(buffer())
+	    .pipe(sourcemaps.init({loadMaps: true}))
+	    	.pipe(jshint())
+	    	.pipe(jshint.reporter(stylish))
+	        .pipe(uglify())
+	        .on('error', gutil.log)
+	    .pipe(sourcemaps.write(output.maps))
+	    .pipe(gulp.dest(output.javascript))
+	    .pipe(notify('ES6 compiled to JS, Linted, Bundled, and Minified'));
+	});
 
 //Watch and Build
 	//Watch SCSS Folder If Changes Run CSS
 	//Watch JS Folder If Changes Run JS
 
 	gulp.task('watch', function() {
-		gulp.watch(input.es6, ['build-js']);
-		gulp.watch(input.sass, ['build-css']);
+		gulp.watch(input.javascript, ['build-js'])
+		.on('change', function(event) {
+			console.log('ES6 File ' + event.path + ' was ' + event.type + ', running tasks sir!')
+		});
+		gulp.watch(input.sass, ['build-css'])
+		.on('change', function(event) {
+			console.log('Sass File ' + event.path + ' was ' + event.type + ', running tasks sir!')
+		});
 	});

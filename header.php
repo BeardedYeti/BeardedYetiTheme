73f39b771833f9ae0b87c88eb26c50990498118c
Colin Smith <cmsgoboston41@gmail.com>
2016-09-08 21:39:26 -0400

    Add site-logo to header.php

--- header.php
+++ header.php
@@ -30,11 +30,14 @@
 	<header id="masthead" class="site-header" role="banner">
 <?php } ?>
 
-		<!--<?php if ( get_header_image() ) : ?>
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-				<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
+		<?php // Display site icon or first letter as log ?>
+		<div class="site-logo">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"
+			rel="home">
+				<div class="screen-reader-text">Go to the home page of <?php bloginfo( 'name' ); ?></div>
+				Site logo goes here.
 			</a>
-		<?php endif; // End header image check. ?>-->
+		</div>	
 
 		<div class="site-branding">
 			<?php

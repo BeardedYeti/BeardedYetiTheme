763579e91992d842cabf9e03f457f8aa461fd760
Colin Smith <cmsgoboston41@gmail.com>
2016-09-08 22:28:11 -0400

    Add style to site-logo and site-letter

--- header.php
+++ header.php
@@ -32,10 +32,12 @@
 
 		<?php // Display site icon or first letter as log ?>
 		<div class="site-logo">
-			<a href="<?php echo esc_url( home_url( '/' ) ); ?>"
-			rel="home">
+			<?php $site_title = get_bloginfo( 'name' ); ?>
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
 				<div class="screen-reader-text">Go to the home page of <?php bloginfo( 'name' ); ?></div>
-				Site logo goes here.
+				<div class="site-firstletter" aria-hidden="true">
+					<?php echo substr($site_title, 0, 1); ?>				
+				</div>
 			</a>
 		</div>	
 

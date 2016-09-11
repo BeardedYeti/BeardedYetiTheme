5726908fa4c328221c1502f2accf359c2734be62
Colin Smith <cmsgoboston41@gmail.com>
2016-09-11 15:32:24 -0400

    Add menu navigation style and dropdown functionality

--- functions.php
+++ functions.php
@@ -122,7 +122,12 @@ function beardedyeti_scripts() {
 	// Add local fonts
 	wp_enqueue_style( 'beardedyeti-local-fonts', get_template_directory_uri() . '/lib/css/fonts/custom-fonts.css' );
 
-	wp_enqueue_script( 'beardedyeti-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'beardedyeti-navigation', get_template_directory_uri() . '/js/navigation.js', array( 'jquery' ), '20151215', true );
+
+	wp_localize_script( 'beardedyeti-navigation', 'screenReaderText', array(
+		'expand'   => '<span class="screen-reader-text">' . __( 'expand child menu', 'beardedyeti' ) . '</span>',
+		'collapse' => '<span class="screen-reader-text">' . __( 'collapse child menu', 'beardedyetis' ) . '</span>',
+	) );
 
 	wp_enqueue_script( 'beardedyeti-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
 

4581c66ae60f6b1312a896c9628e1ea515632ae8
Colin Smith <cmsgoboston41@gmail.com>
2016-10-02 12:37:32 -0400

    Change single-post metadata

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -25,16 +25,16 @@ function beardedyeti_posted_on() {
 	);
 
 	$posted_on = sprintf(
-		esc_html_x( 'Posted on %s', 'post date', 'beardedyeti' ),
+		esc_html_x( '%s', 'post date', 'beardedyeti' ),
 		'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 	);
 
 	$byline = sprintf(
-		esc_html_x( 'by %s', 'post author', 'beardedyeti' ),
+		esc_html_x( '%s', 'post author', 'beardedyeti' ),
 		'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
 	);
 
-	echo '<span class="posted-on">' . $posted_on . '</span><span class="byline"> ' . $byline . '</span>'; // WPCS: XSS OK.
+	echo '<span class="byline">' . $byline . '</span> // <span class="posted_on"> ' . $posted_on . '</span>'; // WPCS: XSS OK.
 
 }
 endif;
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -6,33 +6,49 @@
  *
  * @package beardedyeti
  */
-
 ?>
+<?php global $first_post; ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
+		<?php 
+		if ( has_post_thumbnail() ) { ?>
+			<figure class="featured-image">
+				<?php if ( $first_post == true ) { ?>
+					<a href="<?php echo esc_url( get_permalink() ); ?>" rel="bookmark">
+						<?php the_post_thumbnail(); ?>
+					</a>
+				<?php } else { 
+					the_post_thumbnail(); 
+				}
+				?>
+			</figure>
+		<?php }
+		?>
+		
+		<?php 
+			if ( $first_post == true ) {
+				the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+			} else {
+				the_title( '<h1 class="entry-title">', '</h1>' ); 
+			}
+		
+		?>
 
+		<?php
+		if ( has_excerpt( $post->ID ) ) {
+			echo '<div class="deck">';
+			echo '<p>' . get_the_excerpt() . '</p>';
+			echo '</div><!-- .deck -->';
+		}
+		?>
+		
 		<div class="entry-meta">
 			<?php beardedyeti_posted_on(); ?>
 		</div><!-- .entry-meta -->
 	</header><!-- .entry-header -->
 
-	<!--post thumbnal options-->
-	<?php
-	if( has_post_thumbnail( ) ){
-		?>
-		<div class="single-feat clearfix">
-			<figure class="single-thumb single-thumb-full">
-				<?php
-				$thumbnail = 'full';
-				the_post_thumbnail( $thumbnail );
-				?>
-			</figure>
-		</div><!-- .single-feat-->
-	<?php
-	}
-	?>
 	<div class="entry-content">
 		<?php the_content(); ?>
 		<?php
